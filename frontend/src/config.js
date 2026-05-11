f663000887723cf606504fac6012879693b90591
Yash Jain <06jainyash@gmail.com>
2026-05-11 23:35:03 +0530

    Switch to CloudFront HTTPS backend URL

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,7 +1,7 @@
 // In development, we use the Vite proxy (defined in vite.config.js)
-// In production, we use the absolute URL of the Elastic Beanstalk backend
+// In production, we use the CloudFront HTTPS wrapper for our EB backend
 const API_BASE_URL = import.meta.env.PROD 
-  ? 'http://student-mgmt-env.eba-rpm2e4pb.us-east-1.elasticbeanstalk.com' 
+  ? 'https://d335ezydcfytbv.cloudfront.net' 
   : '';
 
 export default API_BASE_URL;
